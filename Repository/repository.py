c927ed20afbcf6bd433f74c66eda5b61fb95ec8f
Jacob Melton <jmelton@redhat.com>
2016-10-23 14:35:50 -0400

    Push all the repository code

--- Repository/repository.py
+++ Repository/repository.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 
+import json
+import pymongo
+import pika
 import socket
 import sys
 from time import sleep
 
 from zeroconf import ServiceInfo, Zeroconf
 
+def on_request(ch, method, props, body):
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
+    db = client.repository
+    collection = db.messages
+
+    print(" [x] Received %r" % body)
+    data = json.loads(body.decode())
+    print(data)
+    if data['Action'] == 'pull':
+        print("pulling")
+    if data['Action'] == 'push':
+        print("Adding to mongodb")
+        data.pop('Action', None)
+        # Insert the message into the database
+        result = collection.insert_one(data)
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+
 if __name__ == '__main__':
     desc = {'queue_name': 'myQueue'}
-
     info = ServiceInfo("_amqp._tcp.local.",
                        "rabbitmq._amqp._tcp.local.",
                        socket.inet_aton("127.0.0.1"), 5672, 0, 0,
@@ -17,6 +36,19 @@ if __name__ == '__main__':
     zeroconf = Zeroconf()
     print("Registration of a service, press Ctrl-C to exit...")
     zeroconf.register_service(info)
+
+    credentials = pika.PlainCredentials(username='team16', password='ece4564')
+    connection =  pika.BlockingConnection(pika.ConnectionParameters(host='172.31.61.87',
+                                                                    port=5672,
+                                                                    credentials=credentials))
+    channel = connection.channel()
+    channel.queue_declare(queue='brogrammers')
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(on_request, queue='brogrammers')
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
     try:
         while True:
             sleep(0.1)
