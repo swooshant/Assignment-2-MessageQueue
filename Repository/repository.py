3396c62c1bc1f8f16310c729754b315ce097b9f5
Jacob Melton <jmelton@redhat.com>
2016-10-23 19:27:04 -0400

    Update repository with improved logic for pull actions

--- Repository/repository.py
+++ Repository/repository.py
@@ -19,7 +19,6 @@ def on_request(ch, method, props, body):
     client = pymongo.MongoClient('mongodb://localhost:27017/')
     db = client.repository
     collection = db.messages
-
     myResult = []
 
     print(" [x] Received %r" % body)
@@ -28,66 +27,70 @@ def on_request(ch, method, props, body):
 
     if data['Action'] == 'pull':
         print("pulling")
-        if 'Subject' in data:
+        if 'Subject' in data and 'Message' in data:
             subject = data['Subject']
-            if 'Message' in data:
-                message = data['Message']
-                print("Searching DB")
-                result = collection.find({'Subject':{'$regex':subject},'Message':{'regex':message}})
-            else:
-                print("Searching DB")
-                result = collection.find({'Subject':{'$regex':subject}})
+            messsage = data['Message']
+            print("Searching DB")
+            result = collection.find({'Subject':{'$regex':subject},'Message':{'$regex':message}})
+        elif 'Subject' in data:
+            subject = data['Subject']
+            print("Searching DB")
+            result = collection.find({'Subject':{'$regex':subject}})
         elif 'Message' in data:
             message = data['Message']
             print("Searching DB")
             result = collection.find({'Message':{'$regex':message}})
+        else:
+            print("Shouldnt happen, EVER!")
         print("Sanitizing result")
         for item in list(result):
             item.pop('_id', None)
             myResult.append(item)
-        print(myResult)
         reply = json.dumps(myResult)
+        print(reply)
         ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
                      properties=pika.BasicProperties(correlation_id = \
                                                          props.correlation_id),
                      body=reply)
         ch.basic_ack(delivery_tag = method.delivery_tag)
-
-    if data['Action'] == 'push':
+    elif data['Action'] == 'push':
         print("Adding to mongodb")
         data.pop('Action', None)
         # Insert the message into the database
         result = collection.insert_one(data)
+        gpio.lightLED(collection.count())
         ch.basic_ack(delivery_tag = method.delivery_tag)
+
 if __name__ == '__main__':
+    # Setup GPIO
     gpio.initGPIO()
+    # Setup MongoDB
     client = pymongo.MongoClient('mongodb://localhost:27017/')
     db = client.repository
     collection = db.messages
-
-    gpio.lightLED(collection.count())
-
+    # Setup Zeroconf
     desc = {'queue_name': queue_name, 'username': username, 'password': password}
     info = ServiceInfo("_amqp._tcp.local.",
                        "rabbitmq._amqp._tcp.local.",
                        socket.inet_aton(address), port, 0, 0,
                        desc, "rabbitmq-server.local.")
-
+    # Start Zeroconf
     zeroconf = Zeroconf()
     print("Registration of a service, press Ctrl-C to exit...")
     zeroconf.register_service(info)
-
-    credentials = pika.PlainCredentials(username='team16', password='ece4564')
-    connection =  pika.BlockingConnection(pika.ConnectionParameters(host='172.31.61.87',
-                                                                    port=5672,
+    # Connect to RabbitMQ
+    credentials = pika.PlainCredentials(username=username, password=password)
+    connection =  pika.BlockingConnection(pika.ConnectionParameters(host=address,
+                                                                    port=port,
                                                                     credentials=credentials))
     channel = connection.channel()
     channel.queue_declare(queue='brogrammers')
-
+    # Begin listening to queue
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(on_request, queue='brogrammers')
     print(' [*] Waiting for messages. To exit press CTRL+C')
+    # Start consuming messages
     channel.start_consuming()
 
     try:
