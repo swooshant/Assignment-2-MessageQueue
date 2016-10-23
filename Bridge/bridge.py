a82966db82d3d0373f31f7ac7ddfb208356e2ae9
Jacob Melton <jmelton@redhat.com>
2016-10-23 17:19:10 -0400

    Refactor the bridge code

--- Bridge/bridge.py
+++ Bridge/bridge.py
@@ -1,9 +1,46 @@
+import json
+import pika
 import socket
 import sys
-from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
-import pika
-import json
 import uuid
+
+from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
+
+class BridgeRpcClient(object):
+    def __init__(self):
+        print("Setup Rabbitmq connection")
+        self.credentials = pika.PlainCredentials('team16', 'ece4564')
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('172.31.61.87',
+                                                                             5672,
+                                                                             '/',
+                                                                             self.credentials))
+
+        self.channel = self.connection.channel()
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, blueData, action):
+        if action == "pull":
+            self.response = None
+        else:
+            self.response = "notNone"
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='brogrammers',
+                                   properties=pika.BasicProperties(
+                                        reply_to = self.callback_queue,
+                                        correlation_id = self.corr_id),
+                                   body=blueData)
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
 
 #bluetooth setup
 hostMACAddress = 'B8:27:EB:39:1B:2A' 
@@ -12,84 +49,44 @@ backlog = 10
 size = 4096
 
 #bluetooth
+print("Setting up bluetooth socket")
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 s.bind((hostMACAddress,port))
 s.listen(backlog)
 
-#messageQueue
-credentials = pika.PlainCredentials('team16', 'ece4564')
-connection = pika.BlockingConnection(pika.ConnectionParameters('172.31.61.87', 5672, '/', credentials))
-channel = connection.channel()
-result = channel.queue_declare(exclusive=True)
-callback_queue = result.method.queue
-
-response = None
-corr_id = str()
-
-def on_response(ch, method, props, body):
-		print("In on_response: Print Body: ")
-		print(body)
-		response = body
-
-
-def call(blueData, action):
-		if action == "pull":
-			response = None
-		else:
-			response = "notNone"
-
-		corr_id = str(uuid.uuid4())
-		channel.basic_publish(exchange='', routing_key='brogrammers', properties=pika.BasicProperties(
-                                         reply_to = callback_queue,   correlation_id = corr_id),
-                                   body= blueData)
-		while response is None:
-			connection.process_data_events()
-
-		return 
-
-
-channel.basic_consume(on_response, no_ack=True, queue=callback_queue)
-
-
-print("Stage 1")
+# Setup rpc client
+bridgeRPC = BridgeRpcClient()
 
 try:
-	while 1:
-		client, address = s.accept()
+    while 1:
+        print("Get a  bluetooth connection from client")
+        client, address = s.accept()
 
-		#blueData is bluetooth data
-		blueData = client.recv(size)
-		
-		if blueData:
-			print("stage 2")
+        #blueData is bluetooth data
+        print("Recieve bluetooth data")
+        blueData = client.recv(size)
 
-			print(blueData)
+        if blueData:
+            print(blueData)
 
-			#dictionary of the JSON sent through bluetooth
-			data = json.loads(blueData.decode())
+            #dictionary of the JSON sent through bluetooth
+            data = json.loads(blueData.decode())
 
-			if data['Action'] == "push":
-				print("This is a push")
-				call(blueData, "push")
+            if data['Action'] == "push":
+                print("This is a push")
+                response = bridgeRPC.call(blueData, "push")
+                print(response)
+            elif data['Action'] == "pull":
+                print("This is a pull")
+                response = bridgeRPC.call(blueData, "pull")
+                print(response)
+                client.send(response)
+            else:
+                client.send("Invalid Data Sent")
 
-			elif data['Action'] == "pull":
-				print("This is a pull")
-				call(blueData, "pull")
-				print("In Main: Print Response: ")
-				print(response)
-				client.send(response)
-
-		else:
-			client.send("Invalid Data Sent")
-
-		client.close()
-		s.close()
+        client.close()
 
 except KeyboardInterrupt:
 	print("Closing socket and channel")
 	s.close()
-	connection.close()
-
-		
-
-
+	bridgeRPC.connection.close()
