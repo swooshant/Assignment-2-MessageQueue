5aecef0610335743f866b1184f7131b62db61ce2
Sushant Bhattarai <swooshant@gmail.com>
2016-10-23 19:03:11 -0400

    bridge complete

--- Bridge/bridge.py
+++ Bridge/bridge.py
@@ -1,3 +1,5 @@
+#put code urls here
+
 import json
 import pika
 import socket
@@ -6,14 +8,21 @@ import uuid
 
 from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
 
+
 class BridgeRpcClient(object):
-    def __init__(self):
+
+    def __init__(self, address, port, queue_name, username, password):
+
+        self.queue_name = queue_name
         print("Setup Rabbitmq connection")
-        self.credentials = pika.PlainCredentials('team16', 'ece4564')
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters('172.31.61.87',
-                                                                             5672,
-                                                                             '/',
-                                                                             self.credentials))
+        #self.credentials = pika.PlainCredentials('team16', 'ece4564')
+        #queue_name = 'brogrammers'
+
+        self.credentials = pika.PlainCredentials(username, password)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(address,
+                                                                            port,
+                                                                            '/',
+                                                                            self.credentials))
 
         self.channel = self.connection.channel()
         result = self.channel.queue_declare(exclusive=True)
@@ -31,45 +40,66 @@ class BridgeRpcClient(object):
             self.response = None
         else:
             self.response = "notNone"
+
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(exchange='',
-                                   routing_key='brogrammers',
+                                   routing_key=self.queue_name,
                                    properties=pika.BasicProperties(
-                                        reply_to = self.callback_queue,
-                                        correlation_id = self.corr_id),
+                                       reply_to=self.callback_queue,
+                                       correlation_id=self.corr_id),
                                    body=blueData)
+
         while self.response is None:
             self.connection.process_data_events()
         return self.response
 
-#bluetooth setup
-hostMACAddress = 'B8:27:EB:39:1B:2A' 
+# bluetooth setup
+hostMACAddress = 'B8:27:EB:39:1B:2A'
 port = 25
 backlog = 10
 size = 4096
 
-#bluetooth
+# bluetooth
 print("Setting up bluetooth socket")
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.bind((hostMACAddress,port))
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM,
+                  socket.BTPROTO_RFCOMM)
+s.bind((hostMACAddress, port))
 s.listen(backlog)
 
+# enable zeroconf
+# zeroconf = Zeroconf()
+# print("\nBrowsing services, press Ctrl-C to exit...\n")
+
+# info = zeroconf.get_service_info("_amqp._tcp.local.", "rabbitmq._amqp._tcp.local.")
+
+# address = socket.inet_ntoa(info.address)
+# port = info.port
+# username =  info.properties['username']
+# password =  info.properties['password']
+# queue_name =  info.properties['queue_name']
+
+address = '172.31.61.87'
+port = 5672
+username = 'team16'
+password = 'ece4564'
+queue_name = 'brogrammers'
+
 # Setup rpc client
-bridgeRPC = BridgeRpcClient()
+bridgeRPC = BridgeRpcClient(address, port, queue_name, username, password)
 
 try:
     while 1:
         print("Get a  bluetooth connection from client")
         client, address = s.accept()
 
-        #blueData is bluetooth data
+        # blueData is bluetooth data
         print("Recieve bluetooth data")
         blueData = client.recv(size)
 
         if blueData:
             print(blueData)
 
-            #dictionary of the JSON sent through bluetooth
+            # dictionary of the JSON sent through bluetooth
             data = json.loads(blueData.decode())
 
             if data['Action'] == "push":
@@ -84,9 +114,9 @@ try:
             else:
                 client.send("Invalid Data Sent")
 
-        client.close()
+        # client.close()
 
 except KeyboardInterrupt:
-	print("Closing socket and channel")
-	s.close()
-	bridgeRPC.connection.close()
+    print("Closing socket and channel")
+    s.close()
+    bridgeRPC.connection.close()
